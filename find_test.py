cbce5fed74dc788a5b25f881b8b00dfefd199afe
Allegro <amelie.aylesbury@gmail.com>
2016-11-06 20:00:57 +0000

    Moved search to a function.

--- find_test.py
+++ find_test.py
@@ -1,12 +1,15 @@
-def letter_search():
-    print('Type in a word.')
-    word=raw_input()
-    print('Now type in a letter')
-    letter=raw_input()
+print('Type in a word.')
+word=raw_input()
+print('Now type in a letter')
+letter=raw_input()
+
+def letter_search(letter,word):
     search_pointer=0
     while word.find(letter,search_pointer)!=-1:
         result=word.find(letter,search_pointer )
         print(result)
         search_pointer=result+1
 
-letter_search()
+letter_search('o','school')
+letter_search('e','elephant')
+letter_search(letter,word)
