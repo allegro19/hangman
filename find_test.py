6b67c45a4950d0db05a2040479a81640bae213db
Allegro <amelie.aylesbury@gmail.com>
2016-11-08 18:28:26 +0000

    Making a list.

--- find_test.py
+++ find_test.py
@@ -8,9 +8,10 @@ def letter_search(letter,word):
     search_pointer=0
     while word.find(letter,search_pointer)!=-1:
         result=word.find(letter,search_pointer )
-        print(result)
+#        print(result)
         search_pointer=result+1
-        index_number=[]
+index_number=[letter,word]
+print(index_number)
 letter_search('o','school')
 letter_search('e','elephant')
 letter_search(letter,word)
