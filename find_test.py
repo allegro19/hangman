2713113f37a3af182ba45a8c34447577d4782d72
Allegro <amelie.aylesbury@gmail.com>
2016-11-08 17:30:37 +0000

    adding list

--- find_test.py
+++ find_test.py
@@ -2,6 +2,7 @@ print('Type in a word.')
 word=raw_input()
 print('Now type in a letter')
 letter=raw_input()
+index_number=[]
 
 def letter_search(letter,word):
     search_pointer=0
@@ -9,7 +10,7 @@ def letter_search(letter,word):
         result=word.find(letter,search_pointer )
         print(result)
         search_pointer=result+1
-
+        index_number=[]
 letter_search('o','school')
 letter_search('e','elephant')
 letter_search(letter,word)
