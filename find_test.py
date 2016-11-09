6ecb7c4d769d53d1019a8124522d0993c835bec7
Allegro <amelie.aylesbury@gmail.com>
2016-11-09 17:54:26 +0000

    making a list

--- find_test.py
+++ find_test.py
@@ -1,17 +1,18 @@
-print('Type in a word.')
-word=raw_input()
-print('Now type in a letter')
-letter=raw_input()
-index_number=[]
-
+#print('Type in a word.')
+#word=raw_input()
+#print('Now type in a letter')
+#letter=raw_input()
+#
 def letter_search(letter,word):
+    index_number=[]
     search_pointer=0
     while word.find(letter,search_pointer)!=-1:
         result=word.find(letter,search_pointer )
 #        print(result)
         search_pointer=result+1
-        index_number=[result,search_pointer]
-print(index_number)
+        index_number=[result]
+        return letter
+    searchletter=letter_search
+    index_number=[result,searchletter]
 letter_search('o','school')
-letter_search('e','elephant')
-letter_search(letter,word)
+#letter_search(letter,word)
