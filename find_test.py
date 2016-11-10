0e32f7087320798367342064e09c5c4688e38221
Allegro <amelie.aylesbury@gmail.com>
2016-11-10 19:56:48 +0000

    Puting function in a list.

--- find_test.py
+++ find_test.py
@@ -1,18 +1,19 @@
-#print('Type in a word.')
-#word=raw_input()
-#print('Now type in a letter')
-#letter=raw_input()
-#
+print('Type in a word.')
+word=raw_input()
+print('Now type in a letter')
+letter=raw_input()
+
 def letter_search(letter,word):
     index_number=[]
     search_pointer=0
     while word.find(letter,search_pointer)!=-1:
         result=word.find(letter,search_pointer )
-#        print(result)
         search_pointer=result+1
-        index_number=[result]
-        return result
-    index_number=[result]
-    
-letter_search('o','school')
-#letter_search(letter,word)
+        # index_number=index_number+[result]
+        index_number.append(result)
+    return index_number
+
+
+searchletter=letter_search(letter,word)
+print(searchletter)
+letter_search(letter,word)
