2afdb4df0973e04d8653272d751c2c934880699c
Amelie Aylesbury <amelie@localhost.localdomain>
2016-11-17 20:29:40 +0000

    making simple program

--- hang_man.py
+++ hang_man.py
@@ -10,18 +10,36 @@ while True:
        print(str(lives) + ' lives left')
        print(word)
     if lives <=0:
-        break
-print('GAME OVER!')
 
 
 
-def replace_letters(target,numbers,letter_guess):
+
+def replace_letter(word,number,letter):
+    return word[0:number]+letter+word[number+1:]
+        
+        
+def replace_letters(word,numbers,letter_guess):
     for x in numbers:
-        word=replace_letter(target,x,letter_guess)
+        word=replace_letter(word,x,letter_guess)
         print(word)   
         
     return word
+target = 'parkrun'
+lives = 9
+word ='-'*len(target)
+print('Guess a letter')
+print(word)
+while True:
+    letter_guess = raw_input()
+    lives = lives-1
+    if lives >=1:
+       print(str(lives) + ' lives left')
+       print(word)
+    if lives <=0:
 
-answer=replace_letters(target,[1,2,3],letter_guess)
-print(answer)
+        answer=replace_letters(word,[],letter_guess)
+        print(answer)
 
+        break
+
+print('GAME OVER!')
