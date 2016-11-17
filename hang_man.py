2dd1f229f3541030c59c07ee41763b4f593f029c
Amelie Aylesbury <amelie@localhost.localdomain>
2016-11-17 19:57:40 +0000

    making simple vergen

--- hang_man.py
+++ hang_man.py
@@ -4,7 +4,7 @@ word ='-'*len(target)
 print('Guess a letter')
 print(word)
 while True:
-    leter_guess = raw_input()
+    letter_guess = raw_input()
     lives = lives-1
     if lives >=1:
        print(str(lives) + ' lives left')
@@ -15,30 +15,13 @@ print('GAME OVER!')
 
 
 
+def replace_letters(target,numbers,letter_guess):
+    for x in numbers:
+        word=replace_letter(target,x,letter_guess)
+        print(word)   
+        
+    return word
 
-
-#new part
-print('Type in a word.')
-word=raw_input()
-print('Now type in a letter')
-letter=raw_input()
-
-def letter_search(letter,word):
-    index_number=[]
-    search_pointer=0
-    while word.find(letter,search_pointer)!=-1:
-        result=word.find(letter,search_pointer )
-        search_pointer=result+1
-        # index_number=index_number+[result]
-        index_number.append(result)
-    return index_number
-
-def replace_letter(word,number,letter):
-    return word[0:number]+letter+word[number+1:]
-
-
-
-answer=replace_letter('school',1,'k')
+answer=replace_letters(target,[1,2,3],letter_guess)
 print(answer)
-letter_search(letter,word)
-letter_search(letter,word)
+
